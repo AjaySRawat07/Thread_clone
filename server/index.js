d230f7402807fc0f971546491f6b4fbf79b742de
AjaySRawat07 <ajju246455@gmail.com>
2025-03-20 13:55:56 +0530

    deploying in vercel

---
+++ server/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+dotenv.config();
+const app = express();
+connectDB();
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "http://localhost:5173");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api", router);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+  console.log(`App is listening on PORT : ${port}`);
+});
